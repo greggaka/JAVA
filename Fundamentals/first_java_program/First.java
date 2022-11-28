eb24215f4edaf3314d8b3d9d982c8e5eb89b6086
greggaka <greggakamine@gmail.com>
2022-11-28 09:28:50 -1000

    first commit

---
+++ Fundamentals/first_java_program/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is Gregg A.");
+        System.out.println("I am 29 years old.");
+        System.out.println("My hometown is Honolulu, HI.");
+    }
+}
